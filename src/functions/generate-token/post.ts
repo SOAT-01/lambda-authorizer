002064cd7b0b483fbae72880f66586310b38748e
Carol Pinheiro <caroline@bossabox.com>
2023-10-10 20:07:52 -0300

    feat: added initial validation to when user does not have cpf

--- src/functions/generate-token/post.ts
+++ src/functions/generate-token/post.ts
@@ -1,8 +1,21 @@
 import { APIGatewayEvent } from "aws-lambda";
 import ApiResponse from "../../utils/ApiResponse";
+import jwt from "jsonwebtoken";
 
 const generateUserToken = async (event: APIGatewayEvent) => {
-  return ApiResponse(200, { token: "Generated token" });
+  const body = JSON.parse(event.body || "{}");
+  if (body.cpf) {
+    return ApiResponse(400, { error: "Username is required" });
+  }
+  console.log(
+    "🚀 ~ file: post.ts:11 ~ generateUserToken ~ process.env.PRIVATE_KEY:",
+    process.env.PRIVATE_KEY
+  );
+  const token = jwt.sign({ cpf: body.cpf }, process.env.PRIVATE_KEY, {
+    algorithm: "RS256",
+    expiresIn: "1h",
+  });
+  return ApiResponse(200, { token });
 };
 
 module.exports.api = generateUserToken;
